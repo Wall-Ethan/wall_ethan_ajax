00ea5a23c76c984e6dac7e3e6be18ebcf19429b3
unknown <wallethan528@gail.com>
2023-11-21 19:18:51 -0500

    spinner

--- js/main.js
+++ js/main.js
@@ -6,7 +6,23 @@
 
   const materialTemplate = document.querySelector("#material-template");
   const materialList = document.querySelector("#material-list");
+  const materialWay = document.querySelector("#material-way");
 
+  let spinner = `
+        <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
+            <path fill="#333" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
+              <animateTransform 
+                attributeName="transform" 
+                attributeType="XML" 
+                type="rotate"
+                dur="1s" 
+                from="0 50 50"
+                to="360 50 50" 
+                repeatCount="indefinite" />
+          </path>
+        </svg>`
+  
   //functions
   function modelLoaded() {
     hotspots.forEach(hotspot => {
@@ -15,8 +31,7 @@
   }
 
   function loadInfoBoxes() {
-
-
+   
     fetch("https://swiftpixel.com/earbud/api/infoboxes")
     .then(response => response.json())
     .then(infoBoxes => {
@@ -40,6 +55,7 @@
 
   function loadMaterialInfo() {
 
+    materialWay.innerHTML = spinner;
     fetch("https://swiftpixel.com/earbud/api/materials")
     .then(response => response.json())
     .then(materialListData => {
@@ -59,8 +75,9 @@
       // Append the populated template to the list
       materialList.appendChild(clone);
     });
+    materialWay.innerHTML = ""
     })
-
+    .catch(error => console.error(error));
   }
   loadMaterialInfo()
 
@@ -81,6 +98,5 @@
     hotspot.addEventListener("mouseenter", showInfo);
     hotspot.addEventListener("mouseleave", hideInfo);
   });
-
 })();
 
